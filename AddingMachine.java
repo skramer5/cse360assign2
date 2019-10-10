3356ec8711c85742d4c7a0b0146aaf724e4a6e8c
skramer5 <56378432+skramer5@users.noreply.github.com>
2019-10-09 21:21:47 -0700

    Add files via upload

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,44 +1,33 @@
-//Sebastian Kramer
-//1208937557
-//CSE360
-//Assignment 2
 package cse360assign2;
 
 public class AddingMachine {
-	
 	//declaration of variables
 	private int total;
-	private String toString;
 	
+	//constructor definition
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
-		toString = "0";
 	}
 	
 	public int getTotal () {
-		//returns the current total
-		return total;
+		// returns the total
+		return 0;
 	}
 	
 	public void add (int value) {
-		//adds parameter to total and formats operation
-		total = value + total;
-		toString += "+" + value;
+		//adds value to the parameter
 	}
 	
 	public void subtract (int value) {
-		//subtracts parameter from total and formats operation
-		total = total - value;
-		toString += "-" + value;
+		//subtracts the value from the parameter
 	}
 		
 	public String toString () {
 		//separates all values and operators by a space
-		//return "";
-		return toString;
+		return "";
 	}
 
 	public void clear() {
-	
+		//clears the values
 	}
 }
